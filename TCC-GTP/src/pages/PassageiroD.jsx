2c423c7bafbdf8ba5b23d4ee73de3d263bdf2bc2
TheNobu <samueldosreis1234@gmail.com>
2024-03-21 13:10:09 -0300

    continuação

--- TCC-GTP/src/pages/PassageiroD.jsx
+++ TCC-GTP/src/pages/PassageiroD.jsx
@@ -1,40 +1,66 @@
 import { View,Text, Image, StyleSheet  } from "react-native";
 import { useRoute } from '@react-navigation/native';
+import { Avatar } from "react-native-elements";
+import { Keyboard } from 'react-native';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons"; 
+
+
 
 const style = StyleSheet.create({
     container:{
         flex:1,
-        backgroundColor:'#B5C7F5',
+        backgroundColor:'#fff',
     },
     textNome:{
         alignItems: 'center',
         justifyContent: 'center',
-        fontSize: 30,
+        fontSize: 32,
         fontFamily: 'Inter_600SemiBold',
         height: 40,
-        marginRight:16,
         textAlign:"center",
+        marginTop:12,
     },
     textPonto:{
         alignItems: 'center',
         justifyContent: 'center',
-        fontSize: 22,
+        fontSize: 18,
         fontStyle:"italic",
         height: 28,
-        marginRight:16,
-        textAlign:"center"
+        textAlign:"center",
+        marginTop:10,
     },
     text:{
         alignItems: 'center',
         justifyContent: 'center',
-        fontSize: 30,
+        fontSize:30,
         fontStyle:"italic",
-        height: 80,
-        marginRight:16,
-        textAlign:"center"
+        height:80,
+        textAlign:"center",
+        padding:6
     },
+    avatar:{
+        width:228,
+        height:222,
+        borderRadius:108,
+        marginLeft:96,
+        marginTop:30,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+    },
+    containerText:{
+        borderColor:"#000",
+        borderStyle:'solid',
+        borderWidth:2,
+        borderRadius:20,
+        height:50,
+        marginRight:8,
+        marginLeft:8,
+        marginTop:26,
+    }
     
 })
+
 
 
 const PassageiroD = () => {
@@ -44,14 +70,39 @@ const PassageiroD = () => {
   
     return (
         <View style={style.container}>
-            <Text style={style.textNome}> {` ${item.nome}`}</Text>
-            <Text style={style.textPonto}>{` ${item.ponto}`}</Text>
-            <Text style={style.text}>{`endereço: ${item.endereco}`}</Text>
-            <Text style={style.text}>{`CPF: ${item.cpf}`}</Text>
-            <Text style={style.text}>{`telefone: ${item.telefone}`}</Text>
-            <Text style={style.text}>{`nascimento: ${item.dt_nascimento}`}</Text>
-             
+            <View style={style.avatar}>
+                <Avatar
+                size={220}
+                rounded
+                containerStyle={{
+                borderColor:"#000",
+                borderStyle:'solid',
+                borderWidth:2,
+                }}
+                source={{uri:"https://miro.medium.com/v2/resize:fit:736/0*e2FeM-WKmvdXJs9W.jpg"}}
+                />
+            </View>
+            <View>
+                <View>
+                    <Text style={style.textNome}>{` ${item.nome}`}</Text>
+                </View>
+                <View>
+                    <Text style={style.textPonto}>{` ${item.ponto}`}</Text>
+                </View>
+                <View style={style.containerText}>
+                    <FontAwesomeIcon icon={faMapMarkerAlt} style={{ marginRight: 5 }} />
+                    <Text style={style.text}>{` ${item.endereco}`}</Text>
+                </View>
+                <View style={style.containerText}>
+                    <Text style={style.text}>{`${item.telefone}`}</Text>
+                </View>
+                <View style={style.containerText}>
+                    <Text style={style.text}>{`${item.dt_nascimento}`}</Text>
+                </View>
+            </View>
+            
         </View>
+        
     );
 }
 
--- TCC-GTP/src/pages/Passageiros.jsx
+++ TCC-GTP/src/pages/Passageiros.jsx
@@ -44,7 +44,7 @@ const Passageiros = () => {
     const getApi = async() =>{
         try {
             setLoading(true);
-            const response = await axios.get(`http://192.168.227.31:8080/passageiros/passageiros?nome=${search}`);
+            const response = await axios.get(`http://192.168.196.31:8080/passageiros/passageiros?nome=${search}`);
             setResultsInfo(response.data);
         } catch (error) {
             console.log(error)
